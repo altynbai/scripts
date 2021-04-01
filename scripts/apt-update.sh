707420cee178c5eef8637116bf60507048bbfbb2
altynbai <tauanov.aidos@gmail.com>
2021-04-01 16:02:32 +0600

    initial folder add for github sync

---
+++ scripts/apt-update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Sinple script to update Ubuntu
+
+sudo apt update -yy
+sudo apt dist-upgrade -yy
+
+exit
---
+++ scripts/system-check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# System info showing script
+
+echo "Memory info: "
+free -m 
+
+echo "Disk info: "
+du -h 
+
+echo "Uptime info: "
+uptime
